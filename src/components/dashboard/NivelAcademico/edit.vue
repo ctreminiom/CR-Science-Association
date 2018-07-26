cd0beb9eae73d58a7f4a1493e35861dc72b8861c
aleazofeifam <aleazofeifam@gmail.com>
2018-07-26 16:45:12 -0600

    Edit login image Edited NivelAcademico

--- src/components/dashboard/NivelAcademico/edit.vue
+++ src/components/dashboard/NivelAcademico/edit.vue
@@ -1,7 +1,7 @@
 <template>
 
                      <div>
-                        <div id="NewProject" class="modal" :class="open">
+                        <div id="EditAcademico" class="modal" :class="open">
                           <div class="modal-background"></div>
                           <div class="modal-card">
                               <header class="modal-card-head">
@@ -10,8 +10,8 @@
                               </header>
                               <section class="modal-card-body">
                   
-                                <div class="field">
-                                      <label class="label">Tipo de Consecutivo</label>
+                           <div class="field">
+                                      <label class="label">Grado de escolaridad</label>
                                           <div class="control">
                                                   <div class="select is-rounded">
                                                       <select>
@@ -24,37 +24,12 @@
                                   </div>
                   
                                   <div class="field">
-                                      <label class="label">Prefijo</label>
-                                          <div class="control">
-                                              <input  v-model="prefix" class="input " type="text" placeholder="Text input">
-                                          </div>
-                                  </div>
-                  
-                                  <div class="field">
-                                      <label class="label">Descripcion</label>
+                                      <label class="label">Nombre de carrera</label>
                                           <div class="control">
                                               <textarea class="textarea" placeholder="Descripcion" rows="10"></textarea>
-                                              <input v-model="desc" class="input " type="text" placeholder="Text input">
+                                              <input v-model="career" class="input " type="text" placeholder="Text input">
                                           </div>
                                   </div>
-                  
-                                  <div class="field">
-                                      <label class="label">Rango</label>
-                                          <div class="control">
-                                                  <div class="select is-rounded">
-                                                      <select> <!--Estas opciones se pueden poner en una tabla-->
-                                                        <option>1-100 </option>
-                                                        <option>1-1000 </option>
-                                                        <option>1-10000 </option>
-                                                        <option>1-100000 </option>
-                                                      </select>
-                                                  </div>
-                                          </div>
-                                  </div>
-
-                                
-
-
                               </section>
                               <footer class="modal-card-foot">
                                   <button id="BTN_Guardar" class="button is-success">Save changes</button>
