84f136e0d54ca3433cf0f5488653f0accde58984
rodsal <rodsal16@gmail.com>
2018-08-04 07:55:01 -0600

    Add files via upload

---
+++ src/vuex/modules/errors/getters.js
@@ -0,0 +1,5 @@
+const getters = {
+    users: state => {return state.errors}
+}
+
+export default getters
---
+++ src/vuex/modules/errors/mutations.js
@@ -0,0 +1,10 @@
+const mutations = {
+    ERRORS: (state, json) => {
+        state.errors = json
+    },
+}
+
+
+
+export default mutations
+
