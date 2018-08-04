21665153647ee1df085bbc27110a32f74a5f41be
rodsal <rodsal16@gmail.com>
2018-08-04 07:52:50 -0600

    Add files via upload

---
+++ src/components/C3_Descargas/modals/search.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="columns">
+        <div class="column is-3">
+            
+            <input id="datepicker1" class="input" type="date">
+            
+        </div>
+    
+        <div class="column is-3">
+            
+            <input id="datepicker2" class="input" type="date">
+            
+        </div>
+
+        <Add :open="add_active" @close="close"/>
+
+    </div>
+</template>
+
+
+<style scoped>
+</style>
